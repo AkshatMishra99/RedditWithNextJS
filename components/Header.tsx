ef03c9c5a7fb089e79130b38027dcc70e5377690
Akshat Mishra <akshat.mishra@cropsly.com>
2022-05-22 19:09:22 +0530

    auth functionality complete

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,9 @@ import FeedMenu from './FeedMenu'
 import Personal from './Personal'
 import Profile from './Profile'
 import Burger from './Burger'
+import { signIn, useSession } from 'next-auth/react'
 function Header() {
+  const { data: session } = useSession()
   return (
     <div className="top:0 sticky mb-3 flex min-w-max items-center pb-1 pt-2 pl-5 shadow">
       <div className="mr-5 flex  flex-row items-center justify-start">
@@ -35,9 +37,22 @@ function Header() {
         <div>
           <Personal />
         </div>
-        <div>
-          <Profile />
-        </div>
+        {!session && (
+          <div
+            className="mr-2 hidden cursor-pointer items-center space-x-2 border border-gray-100 p-2 lg:flex"
+            onClick={() => signIn()}
+          >
+            <div className="relative h-5 w-5 flex-shrink-0">
+              <Image src="/reddit-bot.png" layout="fill"></Image>
+            </div>
+            <p className="text-gray-500">Sign In</p>
+          </div>
+        )}
+        {session && (
+          <div>
+            <Profile session={session} />
+          </div>
+        )}
       </div>
       <div>
         <Burger />
--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,37 +1,91 @@
 import React from 'react'
-import FaceIcon from '@mui/icons-material/Face'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
-import IconButton from '@mui/material/IconButton'
 import Badge from '@mui/material/Badge'
 import Image from 'next/image'
-function Profile() {
-  const name = 'MoodConfident'
+import { signOut } from 'next-auth/react'
+import { Session } from 'next-auth'
+import Fade from '@mui/material/Fade'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import { LogoutIcon } from '@heroicons/react/outline'
+interface Props {
+  session: Session
+}
+function Profile(props: Props) {
+  const { session } = props
+  // console.log(session)
+  const name = session?.user?.name
   const karma = '3.4k'
-  return (
-    <div className="mr-3 flex items-center justify-between rounded border-2 border-transparent pr-3 hover:border-gray-50 lg:w-48">
-      <div className="flex items-center justify-start">
-        <div className="p-2">
-          <Badge
-            color="success"
-            variant="dot"
-            overlap="circular"
-            anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'right',
-            }}
-          >
-            <AccountCircleIcon />
-          </Badge>
-        </div>
 
-        <div className="hidden flex-col text-xs lg:flex">
-          <div>{name}</div>
-          <div className="text-somewhat-grey">{karma} karma</div>
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    // console.log(event)
+    if (!anchorEl) setAnchorEl(event.currentTarget)
+    else setAnchorEl(null)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  const handleSignOut = () => {
+    handleClose()
+    signOut()
+  }
+  return (
+    <div>
+      <div
+        id="fade-button"
+        className="mr-3 flex cursor-pointer items-center justify-between rounded border-2 border-transparent pr-3 hover:border-gray-50 lg:w-48"
+        aria-controls={open ? 'fade-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        <div className="flex items-center justify-start">
+          <div className="p-2">
+            <Badge
+              color="success"
+              variant="dot"
+              overlap="circular"
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+              }}
+            >
+              <AccountCircleIcon />
+            </Badge>
+          </div>
+
+          <div className="hidden flex-col text-xs lg:flex">
+            <div className="truncate">{name}</div>
+            <div className="text-somewhat-grey">{karma} karma</div>
+          </div>
+        </div>
+        <div className="">
+          <Image src="/arrowdown.png" height="10" width="10"></Image>
         </div>
       </div>
-      <div className="">
-        <Image src="/arrowdown.png" height="10" width="10"></Image>
-      </div>
+      <Menu
+        className="w-80"
+        id="fade-menu"
+        MenuListProps={{
+          'aria-labelledby': 'fade-button',
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Fade}
+      >
+        <MenuItem
+          className="flex w-[200px] justify-start space-x-3"
+          onClick={handleSignOut}
+        >
+          <div>
+            <LogoutIcon className="h-7 w-7" />
+          </div>
+          <div>Sign Out</div>
+        </MenuItem>
+      </Menu>
     </div>
   )
 }
